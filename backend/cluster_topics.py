d84a7b81c901076940cb4aa890ab95886e91f0d1
Russell Elliott <russell@quadsquad.net>
2026-04-02 12:34:12 -0700

    even more filtering

--- backend/cluster_topics.py
+++ backend/cluster_topics.py
@@ -53,6 +53,7 @@ CUSTOM_CLUSTER_STOP_WORDS = {
     "so", "want", "expect", "actually", "whether", "other", "before", "become",
     "any", "each", "after", "than", "beyond", "ready", "must", "simply",
     "big", "you're", "please", "we're", "agree",
+    "we'll", "jill", "what's", "director", "khosla", "he", "she", "they", "women",
 
     # 6. DOCUMENT META & GEOGRAPHY (Data artifact noise)
     "https", "com", "www", "io", "website", "post", "link", "information",
