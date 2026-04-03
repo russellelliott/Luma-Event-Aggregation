917c9e2b3149e013709960a07dc73db24220b109
Russell Elliott <russell@quadsquad.net>
2026-04-03 12:12:05 -0700

    more filters

--- backend/cluster_topics.py
+++ backend/cluster_topics.py
@@ -75,7 +75,7 @@ CUSTOM_CLUSTER_STOP_WORDS = {
     "taiwan",
     "limited", "hosts", "around",
     "self", "governed", "vertical", "public", "series", "water",
-    "growthx", "bro", "tour", "stage",
+    "growthx", "bro", "tour", "stage", "these",
 
     # 6. DOCUMENT META & GEOGRAPHY (Data artifact noise)
     "https", "com", "www", "io", "website", "post", "link", "information",
